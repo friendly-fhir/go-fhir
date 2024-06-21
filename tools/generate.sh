4e4fb8a3bb65d264c14066d3922f7e28a10aafaf
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2024-06-20 23:24:37 -0400

    Add first-draft of Fhenix templates

---
+++ tools/generate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+readonly REPO_ROOT
+
+if ! command "fhenix" >/dev/null 2>&1; then
+  go install github.com/friendly-fhir/fhenix@latest
+fi
+
+artifact_dir="${REPO_ROOT}/dist"
+readonly artifact_dir
+
+fhenix run --output "${artifact_dir}" .fhenix/r4/config.yaml
+rm -r "${REPO_ROOT}/r4"
+mv "${artifact_dir}/r4" "${REPO_ROOT}/r4"
+go fmt ./r4/...
+rm -r "${artifact_dir}"
